6b51f3e85c5e0b853ae0b587f6af526f3fe68628
coollias <3209918513@qq.com>
2025-04-12 15:11:33 +0800

    add

---
+++ src/main/java/com/tencent/wxcloudrun/controller/pushController.java
@@ -0,0 +1,34 @@
+package com.tencent.wxcloudrun.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.tencent.wxcloudrun.config.ApiResponse;
+import com.tencent.wxcloudrun.dto.CounterRequest;
+import com.tencent.wxcloudrun.model.Counter;
+import com.tencent.wxcloudrun.service.CounterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.List;
+
+/**
+ * counter控制器
+ */
+@RestController
+
+public class pushController {
+
+
+
+    @PostMapping(value = "/api/push" ,produces = "application/xml;charset=UTF-8")
+    ApiResponse create( @RequestBody String encryptedMsg) {
+
+        return null;
+    }
+
+}
